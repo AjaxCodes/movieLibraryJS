97bb8ceae88e3312d7495c02f972c09e07be7dd0
Aj Steinbacher <adamjsteinbacher@gmail.com>
2020-10-14 12:53:49 -0500

    get request successful, postmon nodemon , express working and set up

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, function() {
-    consile.log('Server started. Listening on port 3000.');
+    console.log('Server started. Listening on port 3000.');
 });
 
 app.get("/", (req, res) => {
